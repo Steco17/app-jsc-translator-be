63383fcd1e473b345c9ce941478d83d941a9b33f
Egbewatt Kokou Mwangah Smith <ksupro1@gmail.com>
2024-05-04 15:30:08 -0600

    find or create ecr

--- ci/scripts/build-and-push-lambda-image.sh
+++ ci/scripts/build-and-push-lambda-image.sh
@@ -21,13 +21,20 @@ DOCKERFILE="${6}"
 LAMBDA_DIR="$(cd "${1}"; pwd -P)"
 pushd ${LAMBDA_DIR} > /dev/null
 
-
-# LOGIN TO ECR
+# # --------------------------------------------------------------LOCAL-----------------------------------------------------------------------------
+# # LOGIN TO ECR
 # aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REGISTRY
+# # Check if the repository already exists
+# repo_exists=$(aws ecr describe-repositories --repository-names $LAMBDA_NAME --region $REGION --output text --query 'repositories[*].repositoryName')
+# if [ -z "$repo_exists" ]; then 
+#     aws ecr create-repository --repository-name $LAMBDA_NAME --region $REGION 
+# fi
+# # ------------------------------------------------------------------------------------------------------------------------------------------------
 
 # DOCKER BUILD & PUSH
 echo Docker Build and Push
-PUSH_TAG=$REGISTRY/${LAMBDA_NAME}:$IMAGE_TAG
+# PUSH_TAG=$REGISTRY/${LAMBDA_NAME}:$IMAGE_TAG
+PUSH_TAG=$REGISTRY/${LAMBDA_NAME}:latest
 echo $PUSH_TAG
-docker build --build-arg LAMBDA_NAME=${LAMBDA_NAME} -t $IMAGE_TAG --no-cache=true -f $DOCKERFILE .
+docker build --build-arg LAMBDA_NAME=${LAMBDA_NAME} -t $IMAGE_TAG -t $PUSH_TAG --no-cache=true -f $DOCKERFILE .
 docker push $PUSH_TAG
