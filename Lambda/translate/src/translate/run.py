f0957f324092c2e3884a9cffe5179e7c68832c3d
Suh Valentine N. <suhvalentine@gmail.com>
2024-06-10 04:35:03 +0100

    CORS issue

--- Lambda/translate/src/translate/run.py
+++ Lambda/translate/src/translate/run.py
@@ -8,18 +8,18 @@ app = Flask(__name__)
 VALID_API_KEYS = "jsc@20231104"
 
 
-@app.before_request
-def validate_api_key():
-    """
-    Before each request, this function validates the API key.
-    """
-    # Extract the API key from the request headers
-    api_key = request.headers.get("Authorization")
+# @app.before_request
+# def validate_api_key():
+#     """
+#     Before each request, this function validates the API key.
+#     """
+#     # Extract the API key from the request headers
+#     api_key = request.headers.get("Authorization")
 
-    # Check if the API key is provided and matches the expected key
-    if not api_key or api_key != f"Bearer {VALID_API_KEYS}":
-        # If the API key is missing or incorrect, return a 401 Unauthorized response
-        return jsonify({"error": "Unauthorized No key"}), 401
+#     # Check if the API key is provided and matches the expected key
+#     if not api_key or api_key != f"Bearer {VALID_API_KEYS}":
+#         # If the API key is missing or incorrect, return a 401 Unauthorized response
+#         return jsonify({"error": "Unauthorized No key"}), 401
 
 
 @app.route("/")
